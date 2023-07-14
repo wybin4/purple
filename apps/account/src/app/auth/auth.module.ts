2377fa00af158ae9f7ecc16e0f1527c3b2cdae2f
Anna S <anna@mail.com>
2023-07-14 11:42:55 +0300

    14.07.23 восстановление проекта

---
+++ apps/account/src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from '../configs/jwt.config';
+
+@Module({
+  imports: [UserModule, JwtModule.registerAsync(getJWTConfig())],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
