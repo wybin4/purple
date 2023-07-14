d37d41e8d08812676ece05a6d7ea730e574bd5c6
Anna S <anna@mail.com>
2023-07-14 11:51:06 +0300

    14.07.23 первые контракты

--- libs/contracts/src/index.ts
+++ libs/contracts/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/account/account.login';
+export * from './lib/account/account.register';
---
+++ libs/contracts/src/lib/account/account.register.ts
@@ -0,0 +1,13 @@
+export namespace AccountRegister {
+  export const topic = 'account.register.command';
+
+  export class Request {
+    email!: string;
+    password!: string;
+    displayName?: string;
+  }
+
+  export class Response {
+    email!: string;
+  }
+}
