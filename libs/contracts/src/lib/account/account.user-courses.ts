fc3f396d929d75f9fcd2b8db065e5cbd947b75ce
Anna S <anna@mail.com>
2023-07-15 10:57:41 +0300

    15.07.23 новые контракты, продолжение авторизации

---
+++ libs/contracts/src/lib/account/account.user-courses.ts
@@ -0,0 +1,14 @@
+import { IsString } from 'class-validator';
+
+export namespace AccountUserCourses {
+  export const topic = 'account.user-courses.query';
+
+  export class Request {
+    @IsString()
+    id!: string;
+  }
+
+  export class Response {
+    courses!: IUserCourses[];
+  }
+}
---
+++ libs/contracts/src/lib/account/account.user-info.ts
@@ -0,0 +1,14 @@
+import { IsString } from 'class-validator';
+
+export namespace AccountUserInfo {
+  export const topic = 'account.user-info.query';
+
+  export class Request {
+    @IsString()
+    id!: string;
+  }
+
+  export class Response {
+    user!: Omit<IUser, 'passwordHash'>;
+  }
+}
